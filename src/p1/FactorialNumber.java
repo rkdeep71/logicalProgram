fe61df7f16bc0a6dd8bec0e40adb438795eb960e
rkdeep71 <rkdeep4556@gmail.com>
2023-10-10 22:24:35 +0530

    First Commt Wap to swap number

---
+++ src/p1/FactorialNumber.java
@@ -0,0 +1,18 @@
+package p1;
+
+import java.util.Scanner;
+
+public class FactorialNumber {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the number: ");
+		int number = sc.nextInt();
+		int fact = 1;
+
+		for (int i = number; i >= 1; i--) {
+			fact = fact * i;
+
+		}
+		System.out.println("Factorial of " + number + " is " + fact);
+	}
+}
